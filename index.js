00a4086d51f18ac03b44f9fcc6e86dd80460c358
amranhossain <amranhossainchy@gmail.com>
2024-04-08 03:45:28 +0600

    done

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ async function run() {
     await client.connect();
 
     const applicationCollections = client.db('carreerDB').collection('ApplicationDB');
+    const userCollections = client.db('carreerDB').collection('UserDB')
 
     app.get('/applications', async(req, res) => {
         const cursor = applicationCollections.find()
@@ -46,6 +47,27 @@ async function run() {
         res.send(result)
     })
 
+
+    app.get('/users', async(req, res) => {
+      const cursor = userCollections.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+
+    app.post('/users', async(req, res) => {
+      const user = req.body;
+      console.log(user)
+      const result = await userCollections.insertOne(user)
+      res.send(result)
+    })
+
+    app.delete('/users/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const result = await userCollections.deleteOne(query)
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
